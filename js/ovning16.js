4b240bd80b6a683f7ffa7470207c3ee1cb434101
Robert Hansson <roberthanssonr@gmail.com>
2017-02-21 23:16:26 +0100

    Ändrat lite bara

--- js/ovning16.js
+++ js/ovning16.js
@@ -1,17 +1,21 @@
-const urlForWorldApi = list => 'http://forverkliga.se/JavaScript/api/simple.php?world'
+const urlForWorldApi = countryListInformation => 'http://forverkliga.se/JavaScript/api/simple.php?world'
 
-class FetchData extends React.Component {
+class App extends React.Component {
 constructor(props) {
         super(props)
         this.state = {
             requestFailed: false,
             list: []
-        }
+        } 
+        this.handleClickOnList = this.handleClickOnList.bind(this);
 }
-   
+    
+handleClickOnList(event) {
+        this.setState({ list: event.target.value });
+    } 
     
 componentDidMount() {    
-    fetch(urlForWorldApi(this.props.list))
+    fetch(urlForWorldApi(this.props.countryListInformation))
     .then(response => {
         if(!response.ok) {
             throw Error("Network request failed")
@@ -19,16 +23,16 @@ componentDidMount() {
         
         return response
     })
-      .then(d => d.json())
-      .then(d => {
-        console.log(d);
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
         
         this.setState({
-            list: d
+            list: data
         })
         //den skriver ut en lista med object där allt finns.
            
-        console.log(d);
+        console.log(data);
     }, () => {       
         this.setState({
             requestFailed: true
@@ -43,29 +47,52 @@ componentDidMount() {
         else {
             return (
                 <div>
+                    <h3 className='well'>There is {this.state.list.length} countries in the list</h3>
                     <CountryList list={this.state.list}/>
+                <div className="well">
+                                    
                 </div>
-            )
-        }
+                </div>
+            )}
     }
+    //slutet av klassen
 }
 
 function CountryList(props) {
 
-    const countryList = props.list.map((list, index) =>
-            <li key={index} className='list-group-item'>{index + 1}. {list.name} - {list.continent}<button className='pull-right btn-danger' >Delete</button></li>
+    const countryListSorted = props.list.map((list, index) =>
+            <li key={index} value={index} className='list-group-item'>{index + 1}. {list.name} - {list.continent}</li>
     );
     
     return (
         <div>
             <ul className='list-group'>
-                {countryList}           
+                {countryListSorted}           
             </ul>
         </div>       
     );
 }
 
+
 ReactDOM.render(
-  <FetchData />,
+  <App />,
   document.getElementById('root')
 );
+
+/*onItemClick: function (event) {
+
+    this.setState({ selectedItem: event.currentTarget.dataset.id });
+    //where 'id' =  whatever suffix you give the data-* li attribute
+},
+
+render: function() {
+    return (
+        <div>
+            <ul>
+                <li onClick={this.onItemClick} data-id="1" className={this.state.selectedItem == 1 ? "on" : "off"}>Component 1</li>
+                <li onClick={this.onItemClick} data-id="2" className={this.state.selectedItem == 2 ? "on" : "off"}>Component 2</li>
+                <li onClick={this.onItemClick} data-id="3" className={this.state.selectedItem == 3 ? "on" : "off"}>Component 3</li>
+            </ul>
+        </div>
+    );
+},*/
